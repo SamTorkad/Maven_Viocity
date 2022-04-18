39c2a636025e65c39d4665756b1989c7ff06a53d
SamTorkad <103931838+SamTorkad@users.noreply.github.com>
2022-04-18 17:45:28 +0530

    Update Homepage.java

--- src/main/java/Page/Homepage.java
+++ src/main/java/Page/Homepage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Homepage {
-
+// this  is homepage class 
 	@FindBy(xpath = "//*[ @id='branding']//a[@id='welcome']")
 	private WebElement link;
 
